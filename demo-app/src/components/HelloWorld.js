5676c720d8dbf065f1cb645fc3033e60ecec7676
Eric Greene <eric@t4d.io>
2019-06-24 11:30:12 -0700

    add code demos

---
+++ demo-app/src/components/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const HelloWorld = () => {
+  // return React.createElement('h1', null, 'Hello World!');
+  return <div>
+    <h1>Hello World!</h1>
+    <span>test</span>
+  </div>;
+};
