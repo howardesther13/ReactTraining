6385e8e6d8eec9acc51807adc1226ff058a9d8e1
Eric Greene <eric@t4d.io>
2019-06-25 07:07:48 -0700

    add code demos

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -9,10 +9,15 @@ const colorList = [
   'pink', 'orange', 'black', 'yellow',
 ];
 
+const carList = [
+  { id: 1, make: 'Ford', model: 'Fusion Hybrid', year: 2018, color: 'silver', price: 30000 },
+  { id: 2, make: 'Tesla', model: 'S', year: 2019, color: 'red', price: 120000 },
+]
+
 ReactDOM.render(
   <>
     <ColorTool colors={colorList} />
-    <CarTool />
+    <CarTool cars={carList} />
   </>,
   document.querySelector('#root'),
 );
